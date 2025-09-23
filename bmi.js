9dd6f2db63c9165932ec3968323fe05d819a5974
a0905114287 <a0905114287@gmail.com>
2025-09-23 15:30:11 +0800

    0923

--- bmi.js
+++ bmi.js
@@ -1,6 +1,8 @@
-var weight=50;
-var height=160;
+const readline = require("readline-sync");
+
+var weight=readline.question("Ur weight? (KG)");
+var height=readline.question("Ur height? (cm)");
 var bmi=weight/((height/100)**2);
 
+
 console.log("Your BMI is: "+bmi);
-console.log(Math.floor(Math.random()*11));
