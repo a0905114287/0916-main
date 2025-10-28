52fe2314cf636a27b4859d5220506d8bc29eb7c4
a0905114287 <a0905114287@gmail.com>
2025-10-28 14:22:25 +0800

    1028

--- 2dArray.js
+++ 2dArray.js
@@ -1,8 +1,11 @@
 var Score=[
-    [70,80,90,60,80],
-    [80,90,70,75,85],
-    [85,85,80,90,80]
-]
+    [1,2,3,4,5,6,7],//row 0
+    [8,9,10,11,12,13,14],//row 1
+    [15,85,80,90,80]// ...
+ //col0, 1, 2,...
+]//3x5 2d array
+
+
 console.log("Score[0]="+Score[0]);
 //avg for every element
 
@@ -12,5 +15,5 @@ var Student=[
     {"name":"tom", "grade":2, "score":[80,90,70,75,85]},
     {"name":"art", "grade":3, "score":[85,85,80,90,80]}
 ]
-console.log("first student'sname="+Student[0].name)
+console.log("first student's name="+Student[0].name)
 //show all student's name & avg
