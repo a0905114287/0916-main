4ffbe4056565631538c2cfa7e8baef85a4710521
a0905114287 <a0905114287@gmail.com>
2025-10-28 16:06:37 +0800

    1028

--- gameLife/Life.js
+++ gameLife/Life.js
@@ -1,3 +1,4 @@
+
 const LIVE=1, DEAD=0
 
 class Life{
@@ -18,20 +19,60 @@ class Life{
         if(_row<0 || _col<0 || _row>=this.row || _col>=this.col){
             return DEAD;
         }else{
-            this.grid[_row][_col]
+            return this.grid[_row][_col]
         }
     }
     neighborCount=function(_row,_col){
         var count=0;
         count += this.getStatusAt(_row-1, _col-1);//left top
-        count += this.getStatusAt(_row-1, _col-1);//up
-        count += this.getStatusAt(_row-1, _col-1);//right top
-        count += this.getStatusAt(_row-1, _col-1);//left
-        count += this.getStatusAt(_row-1, _col-1);//right
-        count += this.getStatusAt(_row-1, _col-1);//left bottom
-        count += this.getStatusAt(_row-1, _col-1);//bottom
-        count += this.getStatusAt(_row-1, _col-1);//right bottom
+        count += this.getStatusAt(_row-1, _col);//up
+        count += this.getStatusAt(_row-1, _col+1);//right top
+        count += this.getStatusAt(_row, _col-1);//left
+        count += this.getStatusAt(_row, _col+1);//right
+        count += this.getStatusAt(_row+1, _col-1);//left bottom
+        count += this.getStatusAt(_row+1, _col);//bottom
+        count += this.getStatusAt(_row+1, _col+1);//right bottom
         return count;
+    }
+    update=function(){
+        //duplicate grid
+        var nextGrid = JSON.parse(JSON.stringify(this.grid));
+        for (let r = 0; r < this.row; r++) {
+            for (let c = 0; c < this.col; c++) {
+               var nCount = this.neighborCount(r, c);
+               //update 
+               //exercise 
+            //    if(this.grid[r][c]==LIVE && (nCount<2 || nCount>3) ) {
+            //         nextGrid[r][c]=DEAD;
+            //    }
+            //    if(this.grid[r][c]==DEAD && nCount==3){
+            //         nextGrid[r][c]=LIVE;
+            //    }
+               if(nCount<2 || nCount>3){
+                   nextGrid[r][c]= (this.grid[r][c]==LIVE) ? DEAD: LIVE;
+               }
+               if(nCount==3)
+                nextGrid[r][c]= (this.grid[r][c]==DEAD) ? LIVE: DEAD;
+            }
+        }
+        this.grid = nextGrid;
+        //garbage collection
+        //gc()
+
+    }
+    //exercise
+    init=function(nLive){
+
+        var randCount=nLive;
+        while(randCount>0){
+           var r=Math.floor(Math.random()*this.row);
+           var c=Math.floor(Math.random()*this.col);
+           if(this.grid[r][c]==DEAD){
+               this.grid[r][c]=LIVE;
+               randCount--;
+           }
+        }
+
     }
 }
 
@@ -50,3 +91,6 @@ class Life{
 
 var myGame1 = new Life(10,10)
 var myGame2 = new Life(5,5)
+myGame1.init(10);
+myGame1.update()
+console.log(myGame1.grid)
