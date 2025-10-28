cd6f1713779ed11f6c73d4c03d90abd4861eb573
a0905114287 <a0905114287@gmail.com>
2025-10-28 16:02:33 +0800

    1028

--- gameLife/Life.js
+++ gameLife/Life.js
@@ -14,7 +14,39 @@ class Life{
         }
 
     }
+    getStatusAt = function(_row,_col){
+        if(_row<0 || _col<0 || _row>=this.row || _col>=this.col){
+            return DEAD;
+        }else{
+            this.grid[_row][_col]
+        }
+    }
+    neighborCount=function(_row,_col){
+        var count=0;
+        count += this.getStatusAt(_row-1, _col-1);//left top
+        count += this.getStatusAt(_row-1, _col-1);//up
+        count += this.getStatusAt(_row-1, _col-1);//right top
+        count += this.getStatusAt(_row-1, _col-1);//left
+        count += this.getStatusAt(_row-1, _col-1);//right
+        count += this.getStatusAt(_row-1, _col-1);//left bottom
+        count += this.getStatusAt(_row-1, _col-1);//bottom
+        count += this.getStatusAt(_row-1, _col-1);//right bottom
+        return count;
+    }
 }
+
+//external
+// Life.prototype.getStatusAt = function(_row,_col){
+//     if(_row<0 || _col<0 || _row>=this.row || _col>=this.col){
+//         return DEAD;
+//     }else{
+//         this.grid[_row][_col]
+//     }
+// }
+
+// function getStatusAt(_row,_col){
+
+// }
 
 var myGame1 = new Life(10,10)
 var myGame2 = new Life(5,5)
